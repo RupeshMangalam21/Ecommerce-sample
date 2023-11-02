f41704235e067fb4485d0c002c25ad85e2fc74fb
Rupesh Mangalam <rupeshm.cse21@gmail.com>
2023-11-02 20:46:33 +0000

    updates

--- pororo/src/Pages/Home.jsx
+++ pororo/src/Pages/Home.jsx
@@ -1,30 +1,44 @@
 import React from 'react';
 import '../styles.css'; // Import your custom CSS file
 import ShopPage from './ShopPage';
-import Landing from '../Images/Landing.mp4';
+import Landing2 from '../Images/Landing2.mp4';
+import sectionbg2 from '../Images/sectionbg2.jpg'
+import { FaShoppingCart } from 'react-icons/fa';
+
+// import Cart from '../Components/Cart';
 const Home = () => {
   return (
     <div className="home-container">
       <header className="header">
-        <h1 className="header-title">Pororo Flowers</h1>
-        <p className="header-subtitle">Your One-Stop-Shop for Fresh Flowers</p>
+        <h1 className="header-title">Sweet Addictions</h1>
+        <p className="header-subtitle">Your One-Stop-Shop for Fresh Cakes and Baked Goodness!</p>
+        <a href='#cart' className='cart-button'>
+        <FaShoppingCart /> _ My Cart
+        </a>
         <a href="#shop" className="header-button">
           Explore Now
         </a>
       </header>
       <section id='landing' className='top-section'>
-        <video src={Landing} autoPlay loop muted className='imgtop'></video>
+        <video src={Landing2} autoPlay loop muted className='imgtop'></video>
+      </section>
+      <section className="section-with-overlay">
+        <div className="overlay"></div>
+        <div className="visit-us">
+          <h2>Visit Us</h2>
+          <p>Sector 15 Market</p>
+          <p>Chandigarh, India</p>
+        </div>
+        {/* <img src={sectionbg2} className='section-bg' alt="Background Image" /> */}
       </section>
       <section id="shop" className="shop-section">
-        <h2 className="shop-title">Shop Our Beautiful Flowers</h2>
+        <h2 className="shop-title">Shop Our Delicious Cakes</h2>
         <ShopPage />
       </section>
 
       <section className="about-section">
         <p className="about-description">
-          At Pororo Flowers, we are dedicated to providing the freshest and most beautiful
-          flowers for every occasion. With a passion for quality and design,
-          we create stunning arrangements that capture the essence of each season.
+        At Sweet Addictions, we are committed to delivering the freshest and most delectable treats for every occasion. With a devotion to quality and creative craftsmanship, we craft delightful confections that capture the essence of each occasion.
         </p>
       </section>
 
--- pororo/src/Pages/ShopPage.jsx
+++ pororo/src/Pages/ShopPage.jsx
@@ -1,32 +1,33 @@
 import React from 'react';
 import ProductCard from '../Components/ProductCard';
 import '../styles.css';
+
 const products = [
   {
     id: 1,
-    title: 'Product 1',
-    description: 'Description of Product 1',
+    title: 'Cream Cheese Fruitcake',
+    description: 'Experience the ultimate fusion of flavors with our Cream Cheese Fruitcake. This sumptuous creation combines the richness of cream cheese with an abundance of luscious, candied fruits. ',
     price: 10.99,
-    imageUrl: 'Product1.jpg'
+    imageUrl: 'Product5.jpg'
   },
   {
     id: 2,
-    title: 'Product 2',
-    description: 'Description of Product 2',
+    title: 'Cherry Vanilla Cake',
+    description: 'Elevate your taste buds with our Cherry Vanilla Cake. This delectable dessert is a celebration of simplicity and sophistication. The light and airy layers of vanilla cake are beautifully complemented by the vibrant burst of cherry flavor.',
     price: 19.99,
     imageUrl: 'Product2.jpg',
   },
   {
     id: 3,
-    title: 'Product 3',
-    description: 'Description of Product 3',
+    title: 'Rich Chocolate Cherry Cake',
+    description: 'Indulge in the decadent delight of our Rich Chocolate Cherry Cake. This exquisite dessert combines the deep, velvety richness of premium chocolate with the sweet and tangy allure of succulent cherries.',
     price: 19.99,
     imageUrl: 'Product3.jpg',
   },
   {
     id: 4,
-    title: 'Product 4',
-    description: 'Description of Product 4',
+    title: 'Box of 3 Pastries',
+    description: 'Choose any three pastries from a collection of original delicacies',
     price: 19.99,
     imageUrl: 'Product4.jpg',
   },
@@ -35,7 +36,7 @@ const products = [
     title: 'Product 5',
     description: 'Description of Product 5',
     price: 19.99,
-    imageUrl: 'Product5.jpg',
+    imageUrl: 'Product1.jpg',
   },
   {
     id: 6,
